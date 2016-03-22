18280829c864d7f95da16039e3c0ae46b05cc2a3
Kevin Jackson <kjackson@thefrontiergroup.com.au>
2016-03-22 15:58:21 +0800

    Added specs for client model

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '@name' do
+    let!(:client)    { FactoryGirl.create(:client) }
+    let(:new_client) { FactoryGirl.build(:client, name: client.name) }
+
+    it { should validate_presence_of(:name) }
+
+    it "validates uniqueness with other clients" do
+      expect(new_client).to be_invalid
+      expect(new_client.errors[:name]).to be_present
+    end
+
+    it "doesn't validate uniqueness with deleted clients" do
+      client.destroy
+      expect(new_client).to be_valid
+    end
+
+    it "doesn't validate uniqueness of name when both clients are deleted" do
+      client.destroy
+      new_client.save!
+      expect(new_client).to be_valid
+    end
+  end
+
+  describe '#to_s' do
+    specify { expect(Client.new(name: "Client").to_s).to eq("Client") }
+  end
+
 end
