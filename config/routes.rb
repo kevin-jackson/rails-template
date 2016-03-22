29cbf5657d89532d4f5309dde020e50c5840836f
Kevin Jackson <kjackson@thefrontiergroup.com.au>
2016-03-22 13:09:20 +0800

    Moved client routes under admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :clients
   # Override registrations controller so we can manually set the role to 'Member'
   devise_for :users, controllers: {
     registrations: "devise_customisations/registrations",
@@ -12,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :dashboard, only: :index
+    resources :clients
     resources :members
     resources :admins
   end
