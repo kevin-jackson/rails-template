ab38f402f042aee7a9b15afc70c16c3310e23e0e
Kevin Jackson <kjackson@thefrontiergroup.com.au>
2016-03-24 14:46:30 +0800

    Removed name search method

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,8 +4,6 @@ class Client < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  scope :name_search, -> (name) { where("name ILIKE ?", "%#{name}%") }
-
   def to_s
     name
   end
